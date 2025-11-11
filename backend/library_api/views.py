cd240936eb2b912f0408594e84181e94131640c2
Alrayan Amer <alrayan004@gmail.com>
2025-11-10 21:56:31 -0600

    Views & URLs

--- backend/library_api/views.py
+++ backend/library_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Author, Book, Loan
+from .serializers import UserSerializer, AuthorSerializer, BookSerializer, LoanSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
